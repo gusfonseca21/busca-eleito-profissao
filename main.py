b4d743370b2779bba9ed6c4906755a205798d06a
Gustavo - CFoco <gusfonseca.dev@gmail.com>
2024-10-15 15:01:01 -0300

    Atualizando filtros

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import os
 import pandas as pd
 
-CAMINHO_DADOS_CANDIDATOS = "./dados/"
+# Filtragem específica para vereadores eleitos em capitais que são policiais
+
+CAMINHO_DADOS_CANDIDATOS = "../../dados/consulta_cand_2024/consulta_cand_2024_BRASIL.csv"
 
 PROFISSAO = 'POLICIAL'
 
+CAPITAIS = ["RIO BRANCO", "MACEIÓ", "MACAPÁ", "MANAUS", "SALVADOR", "FORTALEZA", "VITÓRIA", "GOIÂNIA", "SÃO LUÍS", "CUIABÁ", "CAMPO GRANDE", "BELO HORIZONTE", "BELÉM", "JOÃO PESSOA", "CURITIBA", "RECIFE", "TERESINA", "RIO DE JANEIRO", "NATAL", "PORTO ALEGRE", "PORTO VELHO", "BOA VISTA", "FLORIANÓPOLIS", "SÃO PAULO", "ARACAJU", "PALMAS"]
+
+ESTADOS = ["AC", "AL", "AP", "AM", "BA", "CE", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
+
 resultados_totais = []
 
-for arquivo in os.listdir(CAMINHO_DADOS_CANDIDATOS):
- if arquivo.endswith('.csv'):
-  
-  df = pd.read_csv(os.path.join(CAMINHO_DADOS_CANDIDATOS, arquivo), encoding='latin1', delimiter=';', quotechar='"')
-  
-  resultados = df[df['DS_OCUPACAO'].str.contains(PROFISSAO, na=False) & df['CD_SIT_TOT_TURNO'].isin([1, 2, 3])]
+df = pd.read_csv(CAMINHO_DADOS_CANDIDATOS, encoding='latin1', delimiter=';', quotechar='"')
 
-  resultados_totais.append(resultados)
+resultados = df[df['DS_OCUPACAO'].str.contains(PROFISSAO, na=False) & df['CD_SIT_TOT_TURNO'].isin([2, 3]) & df['NM_UE'].isin(CAPITAIS) & df['SG_UF'].isin(ESTADOS)]
 
-df_final = pd.concat(resultados_totais, ignore_index=True)
-
-df_final.to_csv('resultados.csv', index=False)
+resultados.to_csv('policiais_eleitos_vereadores_capitais_2024_2.csv')
